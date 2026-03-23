dec271289209db2694092aad07aa696e45c19756
Natasha Natape <natapenatasha@gmail.com>
2026-03-23 18:26:29 +0530

    added movies route

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend running 🚀" });
+});
+
+app.get("/movies", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}`
+    );
+
+    const data = await response.json();
+    res.json(data.results);
+
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch movies" });
+  }
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
